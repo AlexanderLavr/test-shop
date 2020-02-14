063d9a6b7f944376464c851ff2e1200a8e52fc58
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-02-14 18:41:26 +0200

    img

--- index.js
+++ index.js
@@ -11,12 +11,12 @@
 import app from './server/App';
 import path from 'path';
 import serveStatic from 'serve-static';
-//import env from './server/config/config';
 import './server/config/db.config';
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-// app.use(express.static('dist'));
+
 let PORT = process.env.PORT || 3300
+
 app.listen(PORT, ()=>{
     console.log(`App is listening on port: ${PORT}`)
 })
