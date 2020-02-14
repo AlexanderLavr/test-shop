2f7b223c7acd823b8a88a17e07e97f07f85be4bb
Alexander <alexander@MacBook-Pro-Alexander.local>
2020-02-14 14:19:47 +0200

    es6

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const path = require('path');
-//const serveStatic = require('serveStatic')
 
 const app = express();
-// app.use()
-app.use(express.static('dist'));
-// app.use('/', serveStatic(path.join(__dirname, '/dist')))
+// app.use(express.static('dist'));
 let PORT = process.env.PORT || 3300
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
